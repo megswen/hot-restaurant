38f00b02b8205062ccd67e123903d881fa2339df
Lindsey Paluso <lindzymarie@gmail.com>
2020-05-16 11:40:48 -0600

    test

--- server.js
+++ server.js
@@ -24,3 +24,26 @@ app.use(express.json());
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
+
+  // Create a set of variables (hint: arrays of objects) for holding the reservation and waitlist data
+
+  var waitlistData = [
+    {
+      routeName: " ",
+      name: " ",
+      phoneNumber: " ",
+      email: " ",
+      uniqueID: " "
+    }
+  ];
+
+  app.post("/tables", function(req, res) {
+    // var newPerson = req.body;
+    // newPerson.routeName = newPerson.name.replace(/\s+/g, "").toLowerCase();
+    // console.log(newPerson);
+
+    // characters.push(newPerson);
+  
+    // res.json(newPerson);
+    console.log(res);
+  });
