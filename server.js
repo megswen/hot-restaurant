68216d89f935f1704f45888f30a5789dd1bd68f7
Lindsey Paluso <lindzymarie@gmail.com>
2020-05-16 11:18:20 -0600

    changed reservations to reserve

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.json());
   app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
   });
-  app.get("/reservations", function(req, res) {
-    res.sendFile(path.join(__dirname, "reservations.html"));
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
   });
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
