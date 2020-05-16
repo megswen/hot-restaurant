dd12a884964fd09570f34f870cbe0915b28d756f
Lindsey Paluso <lindzymarie@gmail.com>
2020-05-16 12:21:54 -0600

    added onclick function

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT | 3004;
+var PORT = process.env.PORT || 3004;
 //hand data parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,29 +21,20 @@ app.use(express.json());
   app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
   });
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+
 
   // Create a set of variables (hint: arrays of objects) for holding the reservation and waitlist data
 
-  var waitlistData = [
-    {
-      routeName: " ",
-      name: " ",
-      phoneNumber: " ",
-      email: " ",
-      uniqueID: " "
-    }
-  ];
+  var waitlistData = [];
 
   app.post("/tables", function(req, res) {
-    // var newPerson = req.body;
-    // newPerson.routeName = newPerson.name.replace(/\s+/g, "").toLowerCase();
-    // console.log(newPerson);
+    console.log(req.body);
+    waitlistData.push(req.body)
+    res.json(waitlistData);
 
-    // characters.push(newPerson);
-  
-    // res.json(newPerson);
-    console.log(res);
+    // console.log(res);
+  });
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
